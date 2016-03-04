62940685302fcbae7e5634534e353416695bc140
kwresch <keenan.wresch@gmail.com>
2016-03-04 10:40:19 -0500

    Added files via upload
    Contains function getLeaderboard(). 1 argument: size of array you want returned. EX: rankarray = getLeaderboard(5) returns top 5 player usernames as array

---
+++ Server/leaderboard.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+
+require "mysql"
+require_relative "db_info"
+
+#returns top 'num' players as Array
+def getLeaderboard(num)
+	begin
+		ranks = Array.new
+		dbc = Mysql.new(@DB_SERVER, @DB_USER, @DB_PASSWORD, @DB_TABLE)
+		q = dbc.query ("SELECT username FROM accounts WHERE rank <= #{num} ORDER BY rank ASC")
+		n_rows = q.num_rows
+		n_rows.times do
+			row = q.fetch_row
+			ranks.push(row[0])
+		end
+		return ranks
+	rescue Mysql::Error => e
+		puts "ERROR"
+		puts "Error Code: #{e.errno}"
+		puts "Error Message: #{e.error}"
+	ensure
+		dbc.close if dbc
+	end
+end
