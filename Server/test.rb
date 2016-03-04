d3ef862a9de7a7a45dc37be1ed5424b088bb91db
kwresch <keenan.wresch@gmail.com>
2016-03-04 02:15:06 -0500

    Create test.rb
    Small test script for account management and game logs.

---
+++ Server/test.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby -w
+
+require_relative 'account'
+require_relative 'access_account'
+require_relative 'log'
+require_relative 'access_logs'
+
+#Test Account
+account = getAccount("johndoe")
+
+puts account.getUsername()
+puts ""
+
+#Test Logs
+log = Log.new("Alice", "Bob")
+log.addToLog("Alice", 1, 10, 11)
+log.addToLog("Bob", 8, 25, 26)
+log.addToLog("Alice", 2, 12, 13)
+log.addToLog("Bob", 9, 28, 29)
+log.setWinner("Alice")
+log.setLoser("Bob")
+log.printLog()
+
+saveLog(log)
+newlog = getLog(0)
+
+newlog.printLog()
+
+#Reset Database
+require_relative 'resetdb'
