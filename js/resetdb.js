9ca04284c80607579b0623e516017809db179a44
Keenan Wresch <keenan.wresch@gmail.com>
2016-04-25 21:29:23 -0400

    Fixed multiline string

--- js/resetdb.js
+++ js/resetdb.js
@@ -3,34 +3,8 @@ var dbconfig = require('db_info');
 
 var connection = mysql.createConnection(dbconfig.connection);
 
-connection.query("DROP TABLE accounts; \
-		DROP TABLE friends; \
-		DROP TABLE loggedin; \
-		DROP TABLE logs;");
+connection.query("DROP TABLE accounts IF EXISTS; DROP TABLE friends IF EXISTS;DROP TABLE loggedin IF EXISTS; DROP TABLE logs IF EXISTS;");
 
-connection.query("CREATE TABLE accounts ( \
-		id INT, \
-	        username VARCHAR(50), \
-	        password VARCHAR(200), \
-	        firstname VARCHAR(30), \
-	        lastname VARCHAR(30), \
-	        score INTEGER, \
-	        games_played INTEGER, \
-	        games_won INTEGER); \
-		\
-		CREATE TABLE friends ( \
-		requester INTEGER, \
-	        receiver INTEGER, \
-	        status INT(1)); \
-		\
-		CREATE TABLE loggedin ( \
-		id INT, \
-        	username VARCHAR(20), \
-        	cookie VARCHAR(200)); \
-		\
-		CREATE TABLE logs ( \
-		timestamp TIMESTAMP(), \
-        	player1 INT, \
-        	player2 INT);");
+connection.query("CREATE TABLE accounts (  id INT UNSIGNED NOT NULL AUTO_INCREMENT, username VARCHAR(50), password VARCHAR(200), email VARCHAR(50), firstname VARCHAR(30), lastname VARCHAR(30), score INTEGER, games_played INTEGER, games_won INTEGER, primary key (id));");
 
 connection.end();
