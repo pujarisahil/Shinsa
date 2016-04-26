f6eab374cde0e5c780a8b47485ab6912672f20e1
Keenan Wresch <keenan.wresch@gmail.com>
2016-04-25 21:37:28 -0400

    Fixed IF EXISTS

--- js/resetdb.js
+++ js/resetdb.js
@@ -3,7 +3,7 @@ var dbconfig = require('../config/database');
 
 var connection = mysql.createConnection(dbconfig.connection);
 
-connection.query("DROP TABLE IF EXISTS accounts; DROP TABLE IF EXISTS friends;DROP TABLE IF EXISTS loggedin; DROP TABLE IF EXISTS logs;");
+connection.query("DROP TABLE IF EXISTS accounts; DROP TABLE IF EXISTS friends; DROP TABLE IF EXISTS loggedin; DROP TABLE IF EXISTS logs;");
 
 connection.query("CREATE TABLE accounts (  id INT UNSIGNED NOT NULL AUTO_INCREMENT, username VARCHAR(50), password VARCHAR(200), email VARCHAR(50), firstname VARCHAR(30), lastname VARCHAR(30), score INTEGER, games_played INTEGER, games_won INTEGER, primary key (id));");
 
